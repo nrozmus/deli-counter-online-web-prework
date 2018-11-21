3cbd09ecf1847816c14691af156a12bbaa9e8440
Nicholas Rozmus <rozmusnicholas@gmail.com>
2018-11-21 20:30:02 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,20 +1,24 @@
 function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(name)
-   return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
-}
- ////
- function nowServing(katzDeliLine) {
-  if (katzDeliLine.length == 0) {
-    return `There is nobody waiting to be served!`
-  }
-  return `Currently serving ${katzDeliLine.shift()}.`
+  var numLine = katzDeliLine.indexOf(name)
+  var welcomeMessage = `Welcome, ${name}. You are number ${numLine + 1} in line.`
+  return welcomeMessage
  }
- ////
- function currentLine(katzDeliLine) {
-  if (katzDeliLine.length == 0) {
-    return `The line is currently empty.`
+ function nowServing(katzDeliLine) {
+  if (katzDeliLine.length != 0) {
+    var firstPerson = katzDeliLine.slice(0,1)
+    katzDeliLine.shift()
+    return `Currently serving ${firstPerson}.`
+  } else {
+    return "There is nobody waiting to be served!"
   }
-  else {
-    return `The line is currently: 1. ` + katzDeliLine[0] +`, 2. ` + katzDeliLine[1] +`, 3. ` + katzDeliLine[2];
+}
+//
+takeANumber(3,"john")
+ function currentLine(currentPosition) {
+  if (currentPosition.length > 0) {
+    return `The line is currently: 1. ${currentPosition[0]}, 2. ${currentPosition[1]}, 3. ${currentPosition[2]}`
+  } else {
+    return 'The line is currently empty.'
   }
 }
